fa362c59feeb2eaa1a92fd8dd22517162a3e2594
Lokesh Telange <149856169+lokeshtelange@users.noreply.github.com>
2024-03-15 19:07:03 +0530

    Update main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
 }
 
 #get vpc data
-data "aws_vpc" "default" {
+data "project-vpc" "default" {
   default = true
 }
 #get public subnets for cluster

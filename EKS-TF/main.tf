e9b4bb4943df845c9fc08fe6930763f90a38a09c
Lokesh Telange <149856169+lokeshtelange@users.noreply.github.com>
2024-03-15 23:20:37 +0530

    Update main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -22,6 +22,65 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
 }
 
 #get vpc data
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create a subnet in the VPC
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"  # Replace with your desired availability zone
+}
+
+# Create another subnet in the VPC
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"  # Replace with your desired availability zone
+}
+
+# Create a route table for the VPC
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "your_internet_gateway_id"  # Replace with your internet gateway ID
+  }
+}
+
+# Associate subnets with the route table
+resource "aws_route_table_association" "subnet1_association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet2_association" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #get public subnets for cluster
 data "aws_subnets" "public" {
